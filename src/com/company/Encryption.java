856ba397cd28fe98b4c971a5c224cf2f6ae2cedb
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-10-16 08:50:54 -0400

    Create Encryption.java

---
+++ src/com/company/Encryption.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by aa700 on 10/16/17.
+ */
+public class Encryption {
+    private double Dkey = Math.random() * (Math.random() * 10);
+    private int key = (int)Dkey;
+
+    public Encryption(String word) {
+        String cipher = word;
+        int length = word.length();
+        for (int i=0; i<length; i++) {
+
+        }
+
+    }
+}
