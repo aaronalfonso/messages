152173e3473ac0a6f1bd5fbd7a01622089d6a252
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-10-16 08:51:05 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner userInput = new Scanner(System.in);
+        String input = userInput.nextLine();
+        Encryption encrypt = new Encryption(input);
     }
 }
