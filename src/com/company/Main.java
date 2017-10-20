fc4c812a750597da145ef4d907069fb4662d52f7
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-10-19 20:55:24 -0400

    i have to go to the bathroom brb

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter a message to encrypt.");
         String input = userInput.nextLine();
         Encryption encrypt = new Encryption(input);
+        System.out.println("Your key is " + encrypt.getKey() + ". Use this key to decrypt your message.");
+        System.out.println("");
+        System.out.println("Your encrypted message is " + encrypt.encrypt(input) + ". Enter your key provided to decrypt.");
+        int key = userInput.nextInt();
+        System.out.println("Your decrypted message is " + encrypt.decrypt(key) + ".");
+
     }
 }
